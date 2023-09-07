55447ff4a1edb61eb5f534c2d9e74133851c9dd2
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2023-09-07 14:30:53 -0700

    Add a refractor_config.py to replace the forward model in muses-py with ReFRACtor versions

---
+++ muses_config/refractor_config.py
@@ -0,0 +1,18 @@
+# These are the arguments passed to cli function that loads this code.
+from py_retrieve.cli import params
+from refractor.muses import RefractorMusesIntegration
+from refractor.tropomi import TropomiInstrumentHandle
+from refractor.omi import OmiInstrumentHandle
+
+# Configuration to use our own TROPOMI and OMI forward models.
+
+r = RefractorMusesIntegration()
+r.instrument_handle_set.add_handle(TropomiInstrumentHandle(use_pca=False,
+                                       use_lrad=False, lrad_second_order=False),
+                                       priority_order=100)
+r.instrument_handle_set.add_handle(OmiInstrumentHandle(use_pca=False,
+                                       use_lrad=False, lrad_second_order=False),
+                                       priority_order=100)
+
+# Replace run_retrieval and run_forward_model with RefractorMusesIntegration
+r.register_with_muses_py()
