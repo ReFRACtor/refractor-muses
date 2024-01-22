79da78876428211d01bfa88471bded01406431ec
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2024-01-22 10:36:26 -0800

    Add in unit test for Sebastians AIRS/OMI EOF case. Right now this is hardcoded, we can either switch the data used to our existing AIRS/OMI case or put Sebastians test case into our refractor_test_data repository in the future.

--- test/omi_eof_test.py
+++ test/omi_eof_test.py
@@ -64,7 +64,52 @@ def test_eof_omi(osp_dir, gmao_dir, vlidort_cli,
         # Print out output of EOF, just so we have something to see
         subprocess.run("h5dump -d OMI_EOF_UV1 -A 0 omi_eof/20160414_23_394_11_23/Products/Products_L2-O3-0.nc", shell=True)
         subprocess.run("h5dump -d OMI_EOF_UV2 -A 0 omi_eof/20160414_23_394_11_23/Products/Products_L2-O3-0.nc", shell=True)
+
+@long_test
+@require_muses_py
+def test_eof_airs_omi(osp_dir, gmao_dir, vlidort_cli,
+                 clean_up_replacement_function):
+    '''Full run of AIRS/OMI that we can compare the output files. This is not
+    really a unit test, but for convenience we have it here. We don't
+    actually do anything with the data, other than make it available.
+
+    Data goes in the local directory, rather than an isolated one.
+
+    This currently depends on a test case Sebastian has, we can either switch
+    this to our existing AIRS/OMI test case or pull his data in. But for now,
+    just use hard coded paths'''
+    subprocess.run("rm -r airs_omi_eof", shell=True)
+    r = MusesRunDir('/tb/sandbox17/sval/muses_output_eof_application_single/airs_omi/2022-11-01/setup-targets/Global_Survey_Grid_4.0/20221101_028_009_22',
+                    osp_dir, gmao_dir, path_prefix="airs_omi_eof")
+    # Modify the Table.asc to add a EOF element. This is just a short cut,
+    # so we don't need to make a new strategy table. Eventually a new table
+    # will be needed in the OSP directory, but it is too early for that.
+    subprocess.run(f'sed -i -e "s/OMINRADWAVUV1/OMINRADWAVUV1,OMIEOFUV1,OMIEOFUV2/" {r.run_dir}/Table.asc', shell=True)
     
+    rs = RetrievalStrategy(f"{r.run_dir}/Table.asc", vlidort_cli=vlidort_cli)
+    rs.register_with_muses_py()
+    # Save data so we can work on getting output in isolation
+    rscap = RetrievalStrategyCaptureObserver("retrieval_step", "retrieval step")
+    rs.add_observer(rscap)
+    ihandle = OmiInstrumentHandle(use_pca=False, use_lrad=False,
+                                  lrad_second_order=False, use_eof=True,
+                                  eof_dir = "/tb/sandbox21/lkuai/muses/output_py/airs_omi/plot/no_softCal_EOF/EOFout")
+    rs.instrument_handle_set.add_handle(ihandle, priority_order=100)
+    rs.state_element_handle_set.add_handle(SingleSpeciesHandle("OMIEOFUV1", OmiEofStateElement, pass_state=False, name="OMIEOFUV1", number_eof=3))
+    rs.state_element_handle_set.add_handle(SingleSpeciesHandle("OMIEOFUV2", OmiEofStateElement, pass_state=False, name="OMIEOFUV2", number_eof=3))
+    if(False):
+        # We can use CLI and call this like py-retrieve. But this absorbed
+        # errors, so it is a bit harder to debug
+        r.run_retrieval(vlidort_cli=vlidort_cli)
+    else:
+        # Instead we can just directly call the function that run_retrieval
+        # ends up running, and occasionally test the other interface to make sure
+        # nothing weird happens
+        rs.retrieval_ms()
+    if True:
+        # Print out output of EOF, just so we have something to see
+        subprocess.run("h5dump -d OMI_EOF_UV1 -A 0 airs_omi_eof/20160414_23_394_11_23/Products/Products_L2-O3-0.nc", shell=True)
+        subprocess.run("h5dump -d OMI_EOF_UV2 -A 0 airs_omi_eof/20160414_23_394_11_23/Products/Products_L2-O3-0.nc", shell=True)
 
 
 # This depends on the previous test. We can skip this test in the future, this
