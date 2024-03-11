dff7a5f8da21a1c99afb82499a23c6a83e17fb22
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2024-03-11 11:54:29 -0700

    Checkpoint work on MusesTropomiObservationNew. Still working on this, but we've made some progress to go ahead and save that.

--- python/refractor/muses/muses_observation.py
+++ python/refractor/muses/muses_observation.py
@@ -21,14 +21,16 @@ class MusesObservation(rf.ObservationSvImpBase):
 
     We may modify this over time, but this is at least a good place to start.
     '''
-    def __init__(self, muses_py_dict, sdesc):
+    def __init__(self, muses_py_dict, sdesc, num_channels=1):
         super().__init__([])
         self.muses_py_dict = muses_py_dict
         self._spectral_window = None
         self._spectral_window_with_bad_sample = None
-        self._sd = [None,]
-        self._spec = [None,]
+        self._num_channels = num_channels
+        self._sd = [None,] * self.num_channels
+        self._spec = [None,] * self.num_channels
         self._sounding_desc = sdesc
+        self._force_no_bad_sample = False
 
     @property
     def sounding_desc(self):
@@ -37,7 +39,7 @@ class MusesObservation(rf.ObservationSvImpBase):
         return self._sounding_desc
         
     def _v_num_channels(self):
-        return 1
+        return self._num_channels
     
     @property
     def spectral_window_with_bad_sample(self):
@@ -61,59 +63,68 @@ class MusesObservation(rf.ObservationSvImpBase):
         e.g., a MusesObservationBase used for one strategy step with a set of microwindows and
         then updated to another set.'''
         self._spectral_window = val
-        self._sd = [None,]
-        self._spec = [None,]
+        self._sd = [None,] * self.num_channels
+        self._spec = [None,] * self.num_channels
 
     def spectral_domain(self, sensor_index):
         return self.radiance(sensor_index).spectral_domain
 
     def radiance_all_with_bad_sample(self):
-        # Assuming just 1 sensor_index, we may need to come back to this
-        freq = self.frequency_full()
-        sindex = np.array(list(range(len(freq)))) + 1
-        sd = rf.SpectralDomain(freq, sindex, rf.Unit("nm"))
-        sr = rf.SpectralRange(self.radiance_full(), rf.Unit("sr^-1"), self.nesr_full())
-        sp = rf.Spectrum(sd, sr)
-        return self.spectral_window_with_bad_sample.apply(sp, 0)
+        try:
+            # "Trick" to leverage radiance_all we already have in StackedRadianceMixin.
+            # We could implement this functionality again, but it is nice just to use
+            # the already tested and implemented StackedRadianceMixin
+            self._force_no_bad_sample = True
+            return self.radiance_all()
+        finally:
+            self._force_no_bad_sample = False
 
     def radiance(self, sensor_index, skip_jacobian=False):
-        # Not sure how to work with sensor_index, we'll need need to sort that out
-        if(sensor_index != 0):
-            raise RuntimeError("Not sure how to handle sensor_index yet")
+        # "Trick" to get radiance_all_with_bad_sample for free. We use the normal
+        # StackedRadianceMixin support, and call radiance_with_bad_sample instead.
+        if(self._force_no_bad_sample):
+            return self.radiance_with_bad_sample(sensor_index)
         if(self._spec[sensor_index] is None):
             # By convention, sample index starts with 1. This was from OCO-2, I'm not
             # sure if that necessarily makes sense here or not. But I think we have code
             # that depends on the 1 base.
-            freq = self.frequency_full()
+            freq = self.frequency_full(sensor_index)
             sindex = np.array(list(range(len(freq)))) + 1
             sd = rf.SpectralDomain(freq, sindex, rf.Unit("nm"))
-            sr = rf.SpectralRange(self.radiance_full(), rf.Unit("sr^-1"), self.nesr_full())
+            sr = rf.SpectralRange(self.radiance_full(sensor_index, skip_jacobian=skip_jacobian),
+                                  rf.Unit("sr^-1"), self.nesr_full(sensor_index))
             sp = rf.Spectrum(sd, sr)
             self._spec[sensor_index] = self.spectral_window.apply(sp, sensor_index)
         return self._spec[sensor_index]
-            
-    def radiance_full(self):
+
+    def radiance_with_bad_sample(self, sensor_index):
+        freq = self.frequency_full(sensor_index)
+        sindex = np.array(list(range(len(freq)))) + 1
+        sd = rf.SpectralDomain(freq, sindex, rf.Unit("nm"))
+        sr = rf.SpectralRange(self.radiance_full(sensor_index), rf.Unit("sr^-1"),
+                              self.nesr_full(sensor_index))
+        sp = rf.Spectrum(sd, sr)
+        return self.spectral_window_with_bad_sample.apply(sp, sensor_index)
+        
+    def radiance_full(self, sensor_index, skip_jacobian=False):
         '''The full list of radiance, before we have removed bad samples or applied the
         microwindows.'''
         raise NotImplementedError
 
-    def frequency_full(self):
+    def frequency_full(self, sensor_index):
         '''The full list of frequency, before we have removed bad samples or applied the
         microwindows.'''
         raise NotImplementedError
 
-    def nesr_full(self):
+    def nesr_full(self, sensor_index):
         '''The full list of NESR, before we have removed bad samples or applied the
         microwindows.'''
         raise NotImplementedError
 
     def bad_sample_mask(self, sensor_index):
-        # Not sure how to work with sensor_index, we'll need need to sort that out
-        if(sensor_index != 0):
-            raise RuntimeError("Not sure how to handle spec_index yet")
         # I think all the radiance data acts the same way, we throw out data with a negative
         # nesr
-        return np.array(self.nesr_full() < 0)
+        return np.array(self.nesr_full(sensor_index) < 0)
 
 class MusesObservationHandle(ObservationHandle):
     '''A lot of our observation classes just map a name to
@@ -195,19 +206,25 @@ class MusesAirsObservationNew(MusesObservation):
             atrack = p['AIRS_ATrack_Index']
             return cls(filename, granule, xtrack, atrack, filter_list)
     
-    def radiance_full(self):
+    def radiance_full(self, sensor_index, skip_jacobian=False):
         '''The full list of radiance, before we have removed bad samples or applied the
         microwindows.'''
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
         return self.muses_py_dict['radiance']['radiance']
 
-    def frequency_full(self):
+    def frequency_full(self, sensor_index):
         '''The full list of frequency, before we have removed bad samples or applied the
         microwindows.'''
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
         return self.muses_py_dict['radiance']['frequency']
 
-    def nesr_full(self):
+    def nesr_full(self, sensor_index):
         '''The full list of NESR, before we have removed bad samples or applied the
         microwindows.'''
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
         return self.muses_py_dict['radiance']['NESR']
 
 
@@ -286,19 +303,25 @@ class MusesCrisObservationNew(MusesObservation):
             pixel_index = p['CRIS_Pixel_Index']
             return cls(filename, granule, xtrack, atrack, pixel_index)
     
-    def radiance_full(self):
+    def radiance_full(self, sensor_index, skip_jacobian=False):
         '''The full list of radiance, before we have removed bad samples or applied the
         microwindows.'''
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
         return self.muses_py_dict['RADIANCE']
 
-    def frequency_full(self):
+    def frequency_full(self, sensor_index):
         '''The full list of frequency, before we have removed bad samples or applied the
         microwindows.'''
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
         return self.muses_py_dict['FREQUENCY']
 
-    def nesr_full(self):
+    def nesr_full(self, sensor_index):
         '''The full list of NESR, before we have removed bad samples or applied the
         microwindows.'''
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
         return self.muses_py_dict['NESR']
 
 # We'll probably pull some of this out, omi and tropomi are similar    
@@ -335,9 +358,62 @@ class MusesTropomiObservationNew(MusesObservation):
         # TODO Fill in POINTINGANGLE_TROPOMI
         for i,flt in enumerate(filter_list):
             sdesc[f'TROPOMI_XTRACK_INDEX_{flt}'] = np.int16(xtrack_list[i])
-            sdesc[f'POINTINGANGLE_TROPOMI_{flt}'] = None
+            # Think this is right
+            sdesc[f'POINTINGANGLE_TROPOMI_{flt}'] = o_tropomi["Earth_Radiance"]["ObservationTable"]["ViewingZenithAngle"][i]
+        self.filter_list = filter_list
         super().__init__(o_tropomi, sdesc)
-    
+
+        # Stash some values we use in later calculations. Note that the radiance data
+        # is all smooshed together, so we separate this.
+        #
+        # It isn't clear here if the best indexing is the full instrument (so 8 bands) with
+        # only some of the bands filled in, or instead the index number into the passed
+        # in filter_list. For now, we are using the index into the filter_list. We can possibly
+        # reevaluate this - it wouldn't be huge change in the code we have here.
+        self._freq_data = []
+        self._nesr_data = []
+        self._bsamp = []
+        for i,flt in enumerate(filter_list):
+            flt_sub = (o_tropomi['Earth_Radiance']['EarthWavelength_Filter'] == flt)
+            self._freq_data.append(o_tropomi['Earth_Radiance']['Wavelength'][flt_sub])
+            self._nesr_data.append(o_tropomi['Earth_Radiance']['EarthRadianceNESR'][flt_sub])
+            self._bsamp.append(
+                (o_tropomi['Earth_Radiance']['EarthRadianceNESR'][flt_sub] <= 0.0)  |
+                 (o_tropomi['Solar_Radiance']['AdjustedSolarRadiance'][flt_sub]<=0.0))
+
+    def spectral_domain(self, sensor_index):
+        # Since self.radiance involves more calculation, give a optimized version of
+        # the spectral_domain function for when we just want this.
+        freq = self.frequency_full(sensor_index)
+        sindex = np.array(list(range(len(freq)))) + 1
+        sd = rf.SpectralDomain(freq, sindex, rf.Unit("nm"))
+        return self.spectral_window.apply(sd, sensor_index)
+        
+    def radiance_full(self, sensor_index, skip_jacobian=False):
+        '''The full list of radiance, before we have removed bad samples or applied the
+        microwindows.'''
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
+        raise NotImplementedError
+
+    def frequency_full(self, sensor_index):
+        '''The full list of frequency, before we have removed bad samples or applied the
+        microwindows.'''
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
+        return self._freq_data[sensor_index]
+
+    def nesr_full(self, sensor_index):
+        '''The full list of NESR, before we have removed bad samples or applied the
+        microwindows.'''
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
+        return self._nesr_data[sensor_index]
+
+    def bad_sample_mask(self, sensor_index):
+        if(sensor_index < 0 or sensor_index >= self.num_channels):
+            raise RuntimeError("sensor_index out of range")
+        return self._bsamp[sensor_index]
     
 ObservationHandleSet.add_default_handle(MusesObservationHandle("AIRS", MusesAirsObservationNew))
 ObservationHandleSet.add_default_handle(MusesObservationHandle("CRIS", MusesCrisObservationNew))
