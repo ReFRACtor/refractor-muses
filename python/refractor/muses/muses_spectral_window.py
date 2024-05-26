088a5f3ca8e3c6a06af8cedd6aba79e7c1480d39
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2024-05-26 15:01:05 -0700

    Add MusesSpectralWindow, which handles including bad samples and full band more cleanly

---
+++ python/refractor/muses/muses_spectral_window.py
@@ -0,0 +1,53 @@
+import refractor.framework as rf
+import copy
+
+class MusesSpectralWindow(rf.SpectralWindow):
+    '''Muses-py retrieval just uses normal SpectralWindow (e.g., a SpectralWindowRange).
+    However there are places where it wants 1) the data restricted to microwindows but
+    including bad pixels (which are otherwise removed with the normal SpectralWindow) and
+    2) the full data (referred to as full band).
+
+    This class adds support for this. It wraps around and existing SpectralWindow and
+    adds flags that can be set to "include_bad_data" or "full_band".
+    '''
+    def __init__(self, spec_win : rf.SpectralWindowRange, obs : 'MusesObservation'):
+        '''Create a MusesSpectralWindow. The passed in spec_win should *not* have
+        bad samples removed. We get the bad sample for the obs passed in and
+        add it to spec_win.
+
+        Right now we only work with a SpectralWindowRange using it's
+        bad_sample_mask. We could extend this if needed - we really
+        just need a SpectralWindow that we can create two versions - a
+        with and without bad sample. But for right now, restrict ourselves to
+        SpectralWindowRange.
+        '''
+        super().__init__()
+        self.include_bad_sample = False
+        self.full_band = False
+        # Let bad samples pass through
+        self._spec_win_with_bad_sample = spec_win
+        swin = copy.deepcopy(spec_win)
+        for i in range(obs.num_channels):
+            swin.bad_sample_mask(obs.bad_sample_mask(i), i)
+        # Remove bad samples
+        self._spec_win = swin
+
+    def _v_number_spectrometer(self):
+        return self._spec_win.number_spectrometer
+
+    def _v_spectral_bound(self):
+        return self._spec_win.spectral_bound
+
+    def desc(self):
+        return "MusesSpectralWindow"
+
+    def grid_indexes(self, grid, spec_index):
+        if(self.full_band):
+            return list(range(grid.data.shape[0]))
+        if(self.include_bad_sample):
+            return self._spec_win_with_bad_sample.grid_indexes(grid, spec_index)
+        return self._spec_win.grid_indexes(grid, spec_index)
+
+__all__ = ["MusesSpectralWindow",]
+           
+    
---
+++ test/muses_spectral_window_test.py
@@ -0,0 +1,40 @@
+from refractor.muses import (MusesSpectralWindow, StrategyTable, MusesOmiObservation)
+import refractor.framework as rf
+from test_support import *
+
+def test_muses_spectral_window(osp_dir):
+    # This is an observation that has some bad samples in it.
+    xtrack_uv1 = 10
+    xtrack_uv2 = 20
+    atrack = 1139
+    filename = f"{joint_omi_test_in_dir}/../OMI-Aura_L1-OML1BRUG_2016m0401t2215-o62308_v003-2016m0402t041806.he4"
+    cld_filename = f"{joint_omi_test_in_dir}/../OMI-Aura_L2-OMCLDO2_2016m0401t2215-o62308_v003-2016m0402t044340.he5"
+    utc_time = "2016-04-01T23:07:33.676106Z"
+    calibration_filename = f"{osp_dir}/OMI/OMI_Rad_Cal/JPL_OMI_RadCaL_2006.h5"
+    stable = StrategyTable(f"{joint_omi_test_in_dir}/Table.asc", osp_dir=osp_dir)
+    obs = MusesOmiObservation(filename, xtrack_uv1, xtrack_uv2, atrack,
+                              utc_time, calibration_filename,
+                              ["UV1", "UV2"],
+                              cld_filename=cld_filename,
+                              osp_dir=osp_dir)
+    step_number = 3
+    # Note this is off by 1. The table numbering get redone after the BT step. It might
+    # be nice to straighten this out - this is actually kind of confusing. Might be better to
+    # just have a way to skip steps - but this is at least how the code works. The
+    # code mpy.modify_from_bt changes the number of steps
+    swin2 = stable.spectral_window("OMI", stp=step_number+1)
+    swin = MusesSpectralWindow(stable.spectral_window("OMI", stp=step_number+1),
+                               obs)
+    spec = obs.spectrum_full(1)
+    # Check number of good points
+    assert swin.apply(spec, 1).spectral_domain.data.shape[0] == 4
+    # Include bad points
+    swin.include_bad_sample = True
+    assert swin.apply(spec, 1).spectral_domain.data.shape[0] == 4+3
+    swin.include_bad_sample = False
+    assert swin.apply(spec, 1).spectral_domain.data.shape[0] == 4
+    # Check number of full band
+    swin.full_band = True
+    assert swin.apply(spec, 1).spectral_domain.data.shape[0] == spec.spectral_domain.data.shape[0]
+    swin.full_band = False
+    assert swin.apply(spec, 1).spectral_domain.data.shape[0] == 4
