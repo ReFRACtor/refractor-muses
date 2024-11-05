dc9ab8852acc1651a1e9457bbbb3978d65a94e16
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2024-11-05 15:52:37 -0800

    Add a version number to the software. We will start with version 0.10, and tag this just to have a starting point. Nothing particularly special about this point, we just have a working system. This can be used as a replacement for py-retrieve, using our new refractor-retrieve program.

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 import os
 from setuptools import setup, find_namespace_packages
 
+# Version moved to python/refractor/muses/version.py so we have one place it is defined.
+exec(open("python/refractor/muses/version.py").read())
+
 # Namespace packages are a bit on the new side. If you haven't seen
 # this before, look at https://packaging.python.org/guides/packaging-namespace-packages/#pkgutil-style-namespace-packages for a description of
 # this.
 setup(
     name='refractor-muses',
-    version='1.0.0',
+    version=__version__,
     description='ReFRACtor MUSE integration',
     author='James McDuffie',
     author_email='James.McDuffie@jpl.nasa.gov',
