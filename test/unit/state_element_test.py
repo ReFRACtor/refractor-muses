2c8412aa37f60d57cf3e76fe9e84fbba50712200
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2025-04-29 11:44:56 -0700

    Change unit test to use simpler interface to test StateElement in isolation from the rest of the code.

--- test/unit/state_element_test.py
+++ test/unit/state_element_test.py
@@ -1,54 +1,43 @@
 from __future__ import annotations
+import refractor.framework as rf  # type: ignore
 from refractor.muses import (
-    RetrievalStrategy,
-    MusesRunDir,
-    ProcessLocation,
     StateElementIdentifier,
+    StateElementOspFile
 )
-from fixtures.misc_fixture import all_output_disabled
-from typing import Any
+import numpy as np
 import numpy.testing as npt
 
-
-class RetrievalStrategyStop:
-    def notify_update(
-        self,
-        retrieval_strategy: RetrievalStrategy,
-        location: ProcessLocation,
-        **kwargs: Any,
-    ) -> None:
-        if location == ProcessLocation("notify_new_step done"):
-            raise StopIteration()
-
-
-def test_omi_state_element(
-    isolated_dir, osp_dir, gmao_dir, vlidort_cli, joint_omi_test_in_dir
-):
-    try:
-        with all_output_disabled():
-            r = MusesRunDir(joint_omi_test_in_dir, osp_dir, gmao_dir, path_prefix=".")
-            rs = RetrievalStrategy(r.run_dir / "Table.asc")
-            rs.clear_observers()
-            rs.add_observer(RetrievalStrategyStop())
-            rs.retrieval_ms()
-    except StopIteration:
-        pass
-    cstate = rs.current_state
-    print([str(i) for i in cstate.full_state_element_id])
-    selem = cstate.full_state_element(StateElementIdentifier("OMIODWAVUV1"))
-    npt.assert_allclose(
-        selem.value,
-        [
-            [
-                0.0,
-            ]
-        ],
-    )
-    npt.assert_allclose(
-        selem.constraint_matrix,
-        [
-            [
-                2500.0,
-            ]
-        ],
-    )
+def test_osp_state_element(osp_dir):
+    apriori_value = np.array([0.0,])
+    latitude = 10.0 # Dummy value, most of the elements don't actually depend on latitude
+    species_directory = osp_dir / "Strategy_Tables" / "ops" / "OSP-OMI-AIRS-v10" / "Species-66"
+    covariance_directory = osp_dir / "Covariance" / "Covariance"
+    selem = StateElementOspFile(StateElementIdentifier("OMIODWAVUV1"), apriori_value,
+                                latitude, species_directory, covariance_directory)
+    # Simulate a state vector update
+    selem.notify_parameter_update(np.array([2.0]))
+    assert selem.retrieval_sv_length == 1
+    assert selem.sys_sv_length == 1
+    assert selem.forward_model_sv_length == 1
+    npt.assert_allclose(selem.value, [2.0])
+    npt.assert_allclose(selem.value_fm, [2.0])
+    npt.assert_allclose(selem.apriori_value, [0.0])
+    npt.assert_allclose(selem.apriori_value_fm, [0.0])
+    cexpect = np.array([[2500.0]])
+    cov_expect = np.array([[0.0004]])
+    npt.assert_allclose(selem.constraint_matrix, cexpect)
+    npt.assert_allclose(selem.apriori_cov_fm, cov_expect)
+    npt.assert_allclose(selem.retrieval_initial_value, [0.0])
+    npt.assert_allclose(selem.step_initial_value, [0.0])
+    npt.assert_allclose(selem.step_initial_value_fm, [0.0])
+    assert selem.true_value is None
+    assert selem.true_value_fm is None
+    assert isinstance(selem.state_mapping, rf.StateMappingLinear)
+    assert isinstance(selem.state_mapping_retrieval_to_fm, rf.StateMappingLinear)
+    # Perhaps basis can go away? Replaced with state_mapping_retrieval_to_fm?
+    npt.assert_allclose(selem.basis_matrix, np.eye(1))
+    npt.assert_allclose(selem.map_to_parameter_matrix, np.eye(1))
+    # notify_start_retrieval
+    # notify_new_step
+    # Can update_state go away? Perhaps notify_solution?
+    
