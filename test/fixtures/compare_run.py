f38e0b1fcf9a0a5690aedcafe49398b569b99615
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2025-05-05 09:56:05 -0700

    Add check that we have the same structure for all our files, e.g. we didn't replace a float with a double or something like that.

--- test/fixtures/compare_run.py
+++ test/fixtures/compare_run.py
@@ -14,28 +14,22 @@ def compare_run(expected_dir, run_dir, diff_is_error=True, from_run_dir=False):
     else:
         dir1 = expected_dir
         dir2 = run_dir
-    for f in glob.glob(f"{dir1}/*/Products/Products_L2*.nc"):
+    flist = glob.glob(f"{dir1}/*/Products/Products_L2*.nc")
+    flist += glob.glob(f"{dir1}/*/Products/Lite_Products_*.nc")
+    flist += glob.glob(f"{dir1}/*/Products/Products_Radiance*.nc")
+    flist += glob.glob(f"{dir1}/*/Products/Products_Jacobian*.nc")
+    flist += glob.glob(f"{dir1}/*/Products/Products_IRK.nc")
+    for f in flist:
         f2 = f.replace(dir1, dir2)
         cmd = f"h5diff --relative 1e-8 {f} {f2}"
         print(cmd, flush=True)
         subprocess.run(cmd, shell=True, check=diff_is_error)
-    for f in glob.glob(f"{dir1}/*/Products/Lite_Products_*.nc"):
-        f2 = f.replace(dir1, dir2)
-        cmd = f"h5diff --relative 1e-8 {f} {f2}"
+        cmd = f"ncdump -c {f} > {f}.struct"
         print(cmd, flush=True)
         subprocess.run(cmd, shell=True, check=diff_is_error)
-    for f in glob.glob(f"{dir1}/*/Products/Products_Radiance*.nc"):
-        f2 = f.replace(dir1, dir2)
-        cmd = f"h5diff --relative 1e-8 {f} {f2}"
+        cmd = f"ncdump -c {f2} > {f}.struct.expected"
         print(cmd, flush=True)
         subprocess.run(cmd, shell=True, check=diff_is_error)
-    for f in glob.glob(f"{dir1}/*/Products/Products_Jacobian*.nc"):
-        f2 = f.replace(dir1, dir2)
-        cmd = f"h5diff --relative 1e-8 {f} {f2}"
-        print(cmd, flush=True)
-        subprocess.run(cmd, shell=True, check=diff_is_error)
-    for f in glob.glob(f"{dir1}/*/Products/Products_IRK.nc"):
-        f2 = f.replace(dir1, dir2)
-        cmd = f"h5diff --relative 1e-8 {f} {f2}"
+        cmd = f"diff -u {f}.struct {f}.struct.expected"
         print(cmd, flush=True)
         subprocess.run(cmd, shell=True, check=diff_is_error)
