c83477419ea052bb30b691f3308d29f2c31c6b01
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2025-05-11 12:31:37 -0700

    Have unit test for CloudResultSummary that actually checks the results.

--- test/unit/cloud_result_summary_test.py
+++ test/unit/cloud_result_summary_test.py
@@ -1,6 +1,7 @@
 import refractor.muses.muses_py as mpy  # type: ignore
 from refractor.muses import CloudResultSummary
 from pytest import approx
+import numpy.testing as npt
 
 def test_cloud_result_summary(joint_tropomi_step_12_output):
     rs, rstp, _ = joint_tropomi_step_12_output
@@ -9,4 +10,24 @@ def test_cloud_result_summary(joint_tropomi_step_12_output):
     result = rstp.results
     error_analysis = rs.error_analysis
     csum = CloudResultSummary(result, error_analysis)
-    #breakpoint()
+    if False:
+        print(csum.cloudODAve)
+        print(csum.cloudODVar)
+        print(csum.cloudODAveError)
+        print(csum.emisDev)
+        print(csum.emissionLayer)
+        print(csum.ozoneCcurve)
+        print(csum.ozone_slope_QA)
+        print(csum.deviation_QA)
+        print(csum.num_deviations_QA)
+        print(csum.DeviationBad_QA)
+    assert csum.cloudODAve == approx(0.5293772644232951)
+    assert csum.cloudODVar == approx(0.7167961581629615)
+    assert csum.cloudODAveError == approx(0.0)
+    assert csum.emisDev == approx(0.0012042654037386358)
+    assert csum.emissionLayer == approx(-6.990282365802727)
+    assert csum.ozoneCcurve == approx(1.0)
+    assert csum.ozone_slope_QA == approx(7.187930140233559)
+    npt.assert_allclose(csum.deviation_QA, [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+    npt.assert_allclose(csum.num_deviations_QA, [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
+    npt.assert_allclose(csum.DeviationBad_QA, [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1])
