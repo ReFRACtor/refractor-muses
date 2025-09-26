af2c09b47579143e4251c84e654b786b3398462d
Mike Smyth <Mike.M.Smyth@jpl.nasa.gov>
2025-09-25 18:14:24 -0700

    Import tropomi and omi code, so we have access to state elements even if we don't have a refractor-config file (which normally handles those imports)

--- test/end_to_end/retrieval_strategy_cris_tropomi_test.py
+++ test/end_to_end/retrieval_strategy_cris_tropomi_test.py
@@ -11,8 +11,8 @@ from refractor.muses import (
 
 # Use refractor forward model, or use py-retrieve.
 # Note that there is a separate set of expected results for a refractor run.
-# run_refractor = False
-run_refractor = True
+run_refractor = False
+# run_refractor = True
 
 # Can use the older py_retrieve matching objects
 match_py_retrieve = False
@@ -81,3 +81,53 @@ def test_retrieval_strategy_cris_tropomi(
         logger.remove(lognum)
     diff_is_error = True
     compare_run(compare_dir, dir, diff_is_error=diff_is_error)
+
+# Don't normally run, we test cris in the cris_tropomi test above. But useful
+# to run to diagnose problems - we've run into issue before with cris only
+# retrievals that don't show up in cris_tropomi
+@pytest.mark.skip
+@pytest.mark.long_test
+def test_retrieval_cris(
+    osp_dir,
+    gmao_dir,
+    python_fp_logger,
+    end_to_end_run_dir,
+    cris_test_in_dir,
+):
+    """Full run, that we then compare the output files to expected results.
+    This is not really a unit test, but for convenience we have it here.
+    Note that a "failure" in the comparison might not actually indicate a problem, just
+    that the output changed. You may need to look into detail and decide that the
+    run was successful and we just want to update the expected results.
+
+    Data goes in the local directory, rather than an isolated one. We can change this
+    in the future if desired, but  for now it is useful to be able to look into the directory
+    if some kind of a problem arises."""
+    dir = end_to_end_run_dir / "retrieval_strategy_cris"
+    subprocess.run(["rm", "-r", str(dir)])
+    r = MusesRunDir(
+        cris_test_in_dir,
+        osp_dir,
+        gmao_dir,
+        path_prefix=dir,
+    )
+    rs = RetrievalStrategy(
+        r.run_dir / "Table.asc",
+        writeOutput=True,
+        writePlots=True,
+    )
+    try:
+        lognum = logger.add(dir / "retrieve.log")
+        # Grab each step so we can separately test output
+        rscap = RetrievalStrategyCaptureObserver(
+            "retrieval_strategy_retrieval_step", "starting run_step"
+        )
+        rs.add_observer(rscap)
+        rscap2 = RetrievalStrategyCaptureObserver(
+            "retrieval_result", "systematic_jacobian"
+        )
+        rs.add_observer(rscap2)
+        rs.retrieval_ms()
+    finally:
+        logger.remove(lognum)
+    # Don't bother checking results, we just want to make sure we can run
