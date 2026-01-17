Continue cleaning up MusesTesObservation. I found a bug in our code, we were passing in a int for the averaging flag, rather than a str of a int value that the tes reader in py-retrieve expected. So we were using the wrong averaging mode. Have that fixed now.

python/refractor/muses/muses_tes_observation.py
@@ -1,5 +1,4 @@
 from __future__ import annotations
-from .misc import osp_setup
 from .observation_handle import ObservationHandleSet
 from .muses_observation import (
     MusesObservationImp,
@@ -7,9 +6,6 @@ from .muses_observation import (
     MeasurementId,
 )
 from .muses_spectral_window import MusesSpectralWindow, TesSpectralWindow
-from .mpy import (
-    mpy_read_tes_l1b,
-)
 import os
 import numpy as np
 import scipy
@@ -19,10 +15,10 @@ from typing import Any, Self
 import typing
 import itertools
 from .identifier import InstrumentIdentifier, FilterIdentifier
+from .input_file_helper import InputFileHelper, InputFilePath
 
 if typing.TYPE_CHECKING:
     from .current_state import CurrentState
-    from .input_file_helper import InputFileHelper, InputFilePath
 
 
 class MusesTesObservation(MusesObservationImp):
@@ -92,21 +88,125 @@ class MusesTesObservation(MusesObservationImp):
     @classmethod
     def read_tes(
         cls,
-        filename: str | os.PathLike[str],
+        filename: str | os.PathLike[str] | InputFilePath,
         l1b_index: list[int],
         l1b_avgflag: int,
         windows: list[dict[str, Any]],
         ifile_hlp: InputFileHelper | None = None,
     ) -> dict[str, Any]:
-        i_fileid = {}
-        i_fileid["preferences"] = {
-            "TES_filename_L1B": os.path.abspath(str(filename)),
-            "TES_filename_L1B_Index": l1b_index,
-            "TES_L1B_Average_Flag": l1b_avgflag,
-        }
-        with osp_setup(ifile_hlp):
-            o_tes = mpy_read_tes_l1b(i_fileid, windows)
+        filters = ['2B1','1B2','2A1','1A1']
+        fpas = ['2B','1B','2A','1A']
+        wavenumberOut = np.zeros((20000),dtype=np.float32)
+        spectraOut = np.zeros((20000),dtype=np.float64)
+        nesrOut = np.zeros((20000),dtype=np.float64)
+        filterOut = np.empty((20000), dtype='<U3')
+
+        count = 0
+        filename = InputFilePath.create_input_file_path(filename)
+        for ifilter, (flt, nmpas, indx) in enumerate(zip(filters, fpas, l1b_index)):
+            with ifile_hlp.open_h5(filename.sub_fname("FP2B", f"FP{nmpas}")) as fh:
+                delta = fh[f"Filter{flt}"].attrs['Delta_Frequency']
+                start = fh[f"Filter{flt}"].attrs['Start_Frequency']
+                n_freq = fh[f"Filter{flt}/NESR"].shape[2]
+                # get all things same across all filters, e.g. surface elevation.
+                if ifilter == 1:
+                    # surface elevation is in index 20.  You can count below in Geolocation or stop the reader and look
+                    # at the variable o_var_array in hdf5_reader.py
+                    geo_info = fh[f"Filter{flt}/Geolocation"][indx]
+
+                    surface_elevation = geo_info[20]
+
+                    # boresightNadirRadians
+                    boresight_nadir_radians = geo_info[30] * np.pi / 180.
+                    # orbitInclinationAngle
+                    orbit_inclination_angle = geo_info[6]
+                    # viewMode
+                    view_mode = 'Nadir'
+                    # instrumentAzimuth
+                    instrument_azimuth = geo_info[33]
+                    # instrumentLatitude
+                    instrument_latitude = geo_info[40]
+                    # geoPointing: not used for nadir
+                    geo_pointing = -999
+                    # targetRadius: not used for nadir
+                    target_radius = -999
+                    # instrumentRadius: not used for nadir
+                    instrument_radius = -999
+                    # orbitAscending
+                    orbit_ascending = geo_info[2]
+                    instrument_altitude = geo_info[42]
+
+                # calculate wavenumber
+                wavenumberOut[count:count + n_freq] = np.array(range(n_freq))*delta + start
+                filterOut[count:count + n_freq] = flt
+                if l1b_avgflag == 0:
+                    # 1 scene
+                    nesr = fh[f"Filter{flt}/NESR"][:,indx,:]
+                    spectra = fh[f"Filter{flt}/Spectra"][:, indx,:]
+                    error = fh[f"Filter{flt}/QA/L1B_Target_Spectra/L1B_General_Error_Flag"][:, indx]
+                    indgood = np.where(error == 0)[0]
+
+                    # do averaging over the good of the 16 pixels
+                    for ii in range(n_freq):
+                        ind = np.where(nesr[indgood,ii] > 0)[0]
+                        if len(ind) > 0:
+                            indgood2 = indgood[ind]
+                            nesrOut[count + ii] = np.sqrt(1/np.sum(1 / nesr[indgood2,ii] / nesr[indgood2,ii]))
+                            spectraOut[count + ii] = np.sum(spectra[indgood2,ii] / nesr[indgood2,ii] / nesr[indgood2,ii]) * nesrOut[count + ii] * nesrOut[count + ii]
+                        else:
+                            # all bad, set nesrOut to negative
+                            nesrOut[count + ii] = np.sqrt(1/np.sum(1 / nesr[indgood,ii] / nesr[indgood,ii]))
+                            spectraOut[count + ii] = np.sum(spectra[indgood,ii] / nesr[indgood,ii] / nesr[indgood,ii]) * nesrOut[count + ii] * nesrOut[count + ii]
+                            nesrOut[count + ii] = -nesrOut[count + ii]
+                else:
+                    # 2 adjacent scenes
+                    nesr = fh[f"Filter{flt}/NESR"][:,indx:indx+2,:]
+                    spectra = fh[f"Filter{flt}/Spectra"][:, indx:indx+2,:]
+                    error = fh[f"Filter{flt}/QA/L1B_Target_Spectra/L1B_General_Error_Flag"][:, indx:indx+2].flatten()
+                    indgood = np.where(error == 0)[0]
+
+                    # do averaging over the good of the 16 pixels and 2 observations 
+                    for ii in range(n_freq):
+                        nx = nesr[:,:,ii].flatten()  # flattened so we can index good detectors
+                        sx = spectra[:,:,ii].flatten() # flattened so we can index good detectors
+                        ind = np.where(nx[indgood] > 0)[0]
+                        if len(ind) > 0:
+                            indgood2 = indgood[ind]
+                            nesrOut[count + ii] = np.sqrt(1/np.sum(1 / nx[indgood2] / nx[indgood2]))
+                            spectraOut[count + ii] = np.sum(sx[indgood] / nx[indgood2] / nx[indgood2]) * nesrOut[count + ii] * nesrOut[count + ii]
+                        else:
+                            # all bad, set nesrOut to negative
+                            nesrOut[count + ii] = np.sqrt(1/np.sum(1 / nx[indgood] / nx[indgood]))
+                            spectraOut[count + ii] = np.sum(sx[indgood] / nx[indgood] / nx[indgood]) * nesrOut[count + ii] * nesrOut[count + ii]
+                            nesrOut[count + ii] = -nesrOut[count + ii]
+
+                count = count + n_freq
+
+        # trim to actual size
+        wavenumberOut = wavenumberOut[0:count]
+        spectraOut = spectraOut[0:count]
+        nesrOut = nesrOut[0:count]
+        filterOut = filterOut[0:count]
+
+        import refractor.muses_py as mpy
+        radianceStruct = mpy.radiance_data(spectraOut, nesrOut, [0], wavenumberOut, filterOut, 'TES')
+
+        o_tes = {'radianceStruct':radianceStruct
+            ,'surfaceElevation':surface_elevation
+            ,'boresightNadirRadians':boresight_nadir_radians
+            ,'orbitInclinationAngle':orbit_inclination_angle
+            ,'viewMode':view_mode
+            ,'instrumentAzimuth':instrument_azimuth
+            ,'instrumentLatitude':instrument_latitude
+            ,'geoPointing':geo_pointing
+            ,'targetRadius':target_radius
+            ,'instrumentRadius':instrument_radius
+            ,'orbitAscending':orbit_ascending
+            ,'instrumentAltitude':instrument_altitude / 1000 # km
+            }
+
         return o_tes
+    
 
     @classmethod
     def _apodization(
@@ -343,7 +443,7 @@ class MusesTesObservation(MusesObservationImp):
             # Read the data from disk, because it doesn't already exist.
             filter_list = mid.filter_list_dict[InstrumentIdentifier("TES")]
             filename = mid["TES_filename_L1B"]
-            l1b_index = mid["TES_filename_L1B_Index"].split(",")
+            l1b_index = [int(i) for i in mid["TES_filename_L1B_Index"].split(",")]
             l1b_avgflag = int(mid["TES_L1B_Average_Flag"])
             run = int(mid["TES_Run"])
             sequence = int(mid["TES_Sequence"])